f77dd4fce0d48c7c112759238449bc7cbc24680c
Santt4na <jhonnsantt4na@gmail.com>
2025-05-16 19:45:10 -0300

    implements Find People by name

--- src/main/java/com/jhonn/santt4na_rest/repository/PersonRepository.java
+++ src/main/java/com/jhonn/santt4na_rest/repository/PersonRepository.java
@@ -1,6 +1,8 @@
 package com.jhonn.santt4na_rest.repository;
 
 import com.jhonn.santt4na_rest.model.Person;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,7 +13,10 @@ import org.springframework.stereotype.Repository;
 public interface PersonRepository extends JpaRepository<Person, Long> {
 	
 	@Modifying(clearAutomatically = true)
-	@Query("UPDATE Person p SET p.enabled = false WHERE p.id = :id")
+	@Query("UPDATE Person p SET p.enabled = false WHERE p.id =:id")
 	void disablePerson(@Param("id") Long id);
+	
+	@Query("SELECT p FROM Person p WHERE p.firstName LIKE LOWER(CONCAT ('%',:firstName,'%'))")
+	Page<Person> findPeopleByName(@Param("firstName") String firstName, Pageable pageable);
 	
 }
