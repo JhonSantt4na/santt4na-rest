e2e0b8c53f8c4ba52963f1873f908262cafc0f3b
Santt4na <jhonnsantt4na@gmail.com>
2025-05-31 22:31:31 -0300

    implements EmailSimple and EmailWithAttachment

---
+++ src/main/java/com/jhonn/santt4na_rest/controllers/EmailController.java
@@ -0,0 +1,37 @@
+package com.jhonn.santt4na_rest.controllers;
+
+import com.jhonn.santt4na_rest.controllers.docs.EmailControllerDocs;
+import com.jhonn.santt4na_rest.dataDTO.v1.request.EmailRequestDTO;
+import com.jhonn.santt4na_rest.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/email/v1")
+public class EmailController implements EmailControllerDocs {
+	
+	@Autowired
+	private EmailService service;
+	
+	
+	@PostMapping()
+	@Override
+	public ResponseEntity<String> sendEmail(@RequestBody EmailRequestDTO emailRequest) {
+		service.sendSimpleEmail(emailRequest);
+		return new ResponseEntity<>("E_mail send with success", HttpStatus.OK);
+	}
+	
+	@PostMapping(value = "/withAttachment", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	@Override
+	public ResponseEntity<String> sendEmailWithAttachment(
+		@RequestParam("emailRequest")  String emailRequest,
+		@RequestParam("attachment") MultipartFile attachment) {
+		
+		service.sendEmailWithAttachment(emailRequest, attachment);
+		return new ResponseEntity<>("Email with attachment send succesfull", HttpStatus.OK);
+	}
+}
---
+++ src/main/java/com/jhonn/santt4na_rest/services/EmailService.java
@@ -0,0 +1,58 @@
+package com.jhonn.santt4na_rest.services;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jhonn.santt4na_rest.config.EmailConfig;
+import com.jhonn.santt4na_rest.dataDTO.v1.request.EmailRequestDTO;
+import com.jhonn.santt4na_rest.mail.EmailSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@Service
+public class EmailService {
+	
+	@Autowired
+	private EmailSender emailSender;
+	
+	@Autowired
+	private EmailConfig emailConfigs;
+	
+	public void sendSimpleEmail(EmailRequestDTO emailRequest) {
+		
+		emailSender
+			.To(emailRequest.getTo())
+			.withSubject(emailRequest.getSubject())
+			.withMessage(emailRequest.getBody())
+			.send(emailConfigs);
+	}
+	
+	public void sendEmailWithAttachment(String emailRequestJson, MultipartFile attachment) {
+		File tempFile = null;
+		try {
+			EmailRequestDTO emailRequest = new ObjectMapper().readValue(emailRequestJson, EmailRequestDTO.class);
+			tempFile = File.createTempFile("attachment", attachment.getOriginalFilename());
+			attachment.transferTo(tempFile);
+			
+			emailSender
+				.To(emailRequest.getTo())
+				.withSubject(emailRequest.getSubject())
+				.withMessage(emailRequest.getBody())
+				.attachment(tempFile.getAbsolutePath())
+				.send(emailConfigs);
+			
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException("Error parsing email request JSON",e);
+		} catch (IOException e) {
+			throw new RuntimeException("Error Processing the attachment",e);
+		}finally {
+			if (tempFile != null && tempFile.exists()){
+				tempFile.delete();
+			}
+		}
+	
+	}
+}
